986901c74fcd1ef233f48041b28517aad1ab0bfd
cheungbh <534660436@qq.com>
2020-04-30 19:29:46 +0800

    mobilenet structure

--- models/mobilenet/MobilePose.py
+++ models/mobilenet/MobilePose.py
@@ -10,26 +10,27 @@ from config.config import train_body_part
 
 n_classes = len(train_body_part)
 
+duc1 = 480
+duc2 = 240
+
 
 def createModel(cfg=None):
     return MobilePose(cfg)
 
 
 class MobilePose(nn.Module):
-    conv_dim = 80
-
     def __init__(self, setting):
         super(MobilePose, self).__init__()
 
         self.mobile = MobileNetV2(inverted_residual_setting=setting)
 
         self.suffle1 = nn.PixelShuffle(2)
-        self.duc1 = DUC(320, 640, upscale_factor=2)
-        self.duc2 = DUC(160, 320, upscale_factor=2)
+        self.duc1 = DUC(320, duc1, upscale_factor=2)
+        self.duc2 = DUC(int(duc1/4), duc2, upscale_factor=2)
         #self.duc3 = DUC(128, 256, upscale_factor=2)
         #self.duc4 = DUC(256, 512, upscale_factor=2)
         self.conv_out = nn.Conv2d(
-            self.conv_dim, n_classes, kernel_size=3, stride=1, padding=1)
+            int(duc2/4), n_classes, kernel_size=3, stride=1, padding=1)
 
     def forward(self, x):
         out = self.mobile(x)
