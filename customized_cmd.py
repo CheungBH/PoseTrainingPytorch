4d39615befa536debb7b2e0a0d86da4682ef0092
SAIL-LAB155 <saillab155@gmail.com>
2020-07-21 19:08:16 +0800

    s2

--- customized_cmd.py
+++ customized_cmd.py
@@ -1,9 +1,9 @@
 with open("training_csv/alphapose_aic.txt", "r") as f:
     lines = [line for line in f.readlines()]
 
-train_begin, train_end = 16, 23
-CUDA = -1
-target_cmds = lines[train_begin: train_end+1]
+train_begin, train_end = 40, 46
+CUDA = 3
+target_cmds = lines[train_begin-1: train_end]
 
 if CUDA != -1:
     cmds = [cmd[:22] + str(CUDA) + cmd[22:-1] + ",\n" for cmd in target_cmds]
