7fb932ab2c6e6e07768c84ff8240272442009037
SAIL-LAB155 <saillab155@gmail.com>
2020-07-25 14:18:52 +0800

    collect res

---
+++ collect_result.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+
+folder_name = "aic_origin"
+src_folder = os.path.join("exp", folder_name)
+dest_folder = os.path.join("result", folder_name)
+os.makedirs(dest_folder, exist_ok=True)
+
+for folder in os.listdir(src_folder):
+    result_folder = os.path.join(src_folder, folder, folder)
+    if os.path.exists(os.path.join(result_folder, "loss.jpg")):
+        shutil.copytree(result_folder, os.path.join(dest_folder, folder))
+
+
+
