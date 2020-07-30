3a2676a68408078d27340dc47a286d25be359aa9
SAIL-LAB155 <saillab155@gmail.com>
2020-07-30 16:06:48 +0800

    move img trt

--- train_result/move_img.py
+++ train_result/move_img.py
@@ -1,7 +1,10 @@
 import os
 import shutil
 
-src = "aic_origin"
+src = "../exp/aic_origin"
 
 for folder in os.listdir(src):
-    shutil.copy(os.path.join(src, folder, "img.jpg"), os.path.join(src, folder, folder, "img.jpg"))
+    try:
+        shutil.copy(os.path.join(src, folder, "img.jpg"), os.path.join(src, folder, folder, "img.jpg"))
+    except:
+        pass
