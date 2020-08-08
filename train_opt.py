2e927a69356fab1ab691888be00eabd65f9255f5
cheungbh <534660436@qq.com>
2020-08-08 21:46:34 +0800

    write csv

--- train_opt.py
+++ train_opt.py
@@ -533,14 +533,15 @@ def main():
         if not exist:
             title_str = "id,backbone,structure,DUC,params,flops,time,loss_param,addDPG,kps,batch_size,optimizer," \
                         "freeze_bn,freeze,sparse,sparse_decay,epoch_num,LR,Gaussian,thresh,weightDecay,loadModel," \
-                        "model_location, folder_name,train_acc,train_loss,val_acc,val_loss,best_epoch,final_epoch"
+                        "model_location, ,folder_name,train_acc,train_loss,val_acc,val_loss,best_epoch,final_epoch"
             title_str = write_decay_title(len(decay_epoch), title_str)
             f.write(title_str)
-        info_str = "{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}, ,{},{},{}\n".format(
-            save_folder, opt.backbone, opt.struct, opt.DUC, params, flops, inf_time, opt.loss_allocate, opt.addDPG,
-            opt.kps, opt.trainBatch, opt.optMethod, opt.freeze_bn, opt.freeze, opt.sparse_s, opt.sparse_decay,
-            opt.nEpochs, opt.LR, opt.hmGauss, opt.ratio, opt.weightDecay, opt.loadModel, config.computer,
-            os.path.join(opt.expFolder, save_folder), train_acc, train_loss, val_acc, val_loss, best_epoch, i)
+        info_str = "{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}, ,{},{},{},{},{},{},{}\n".\
+            format(save_folder, opt.backbone, opt.struct, opt.DUC, params, flops, inf_time, opt.loss_allocate,
+                   opt.addDPG, opt.kps, opt.trainBatch, opt.optMethod, opt.freeze_bn, opt.freeze, opt.sparse_s,
+                   opt.sparse_decay,opt.nEpochs, opt.LR, opt.hmGauss, opt.ratio, opt.weightDecay, opt.loadModel,
+                   config.computer, os.path.join(opt.expFolder, save_folder), train_acc, train_loss, val_acc, val_loss,
+                   best_epoch, i)
         info_str = write_decay_info(decay_epoch, info_str)
         f.write(info_str)
 
