c03afec7e03121369c6d78f7036d5387c0351a2e
cheungbh <534660436@qq.com>
2020-08-10 10:38:06 +0800

    all training table

--- train_result/collect_result.py
+++ train_result/collect_result.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 from train_result.config import task_folder, batch_folder
-from src.config import computer
+from config.config import computer
 
 folder_name = "{}-{}".format(task_folder, batch_folder)
 src_folder = os.path.join("../exp", folder_name)
