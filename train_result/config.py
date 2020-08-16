09280376ef9be1f3872d243c5980da8ab5df89e2
cheungbh <534660436@qq.com>
2020-08-16 20:08:17 +0800

    newest

--- train_result/config.py
+++ train_result/config.py
@@ -1,4 +1,4 @@
-folder_path = "train_result/underwater/mobile_13kps"
+folder_path = "train_result/ceiling/shuffle_13kps"
 folder_path = folder_path.replace("\\", "/")
 
 task_folder = folder_path.split("/")[-2]
