bd2025ac81a6ae21dc46bc2fbd411ac7654509b1
cheungbh <534660436@qq.com>
2020-08-19 10:18:28 +0800

    check train

--- train_result/check_train.py
+++ train_result/check_train.py
@@ -1,6 +1,8 @@
 from train_result.config import task_folder, batch_folder, computer
 import os
 
+generate_remaining = True
+
 result_path = os.path.join("../result", "{0}-{1}/{0}-{1}_result.csv".format(task_folder, batch_folder))
 if not os.path.exists(result_path):
     result_path = os.path.join("../result", "{}-{}_result_{}.csv".format(task_folder, batch_folder, computer))
@@ -27,29 +29,13 @@ print(remain_models)
 wrong_models = [model for model in trained_models if model not in plan_models]
 print(wrong_models)
 
-# trained_folder = "{}/result/log_result".format(models_name)
-#
-# import os
-# file_trained = [int(file) for file in os.listdir(trained_folder)]
-#
-# # print(sorted(trained))
-# rest_file = [item for item in models if item not in file_trained]
-# print("Not in files:")
-# print(rest_file)
-#
-# train_log_name = "alphapose_aic/result/aic_origin_result.xlsx"
-# wb = openpyxl.load_workbook(train_log_name)
-# sheet_names = wb.get_sheet_names()
-# ws = wb.get_sheet_by_name(sheet_names[0])
-# logs = []
-# for row in range(ws.max_row-1):
-#     if ws.cell(row+2,1).value is not None:
-#         logs.append(ws.cell(row+2,1).value)
-# wb.close()
-# print("Not in logs:")
-# rest_log = [item for item in models if item not in logs]
-# print(rest_log)
-#
-# rest_all = [item for item in rest_file if item in rest_log]
-# print("Not in both:")
-# print(rest_all)
+if generate_remaining:
+    file_path = os.path.join(task_folder, batch_folder, "remaining.txt")
+    commands_path = os.path.join(task_folder, batch_folder, "{}.txt".format(batch_folder))
+    with open(commands_path, "r") as f:
+        all_cmd = f.readlines()
+    with open(file_path, "w") as f:
+        for cmd in all_cmd:
+            if len(cmd) > 30:
+                if int(cmd.split(" ")[-1][:-2]) in remain_models:
+                    f.write(cmd)
