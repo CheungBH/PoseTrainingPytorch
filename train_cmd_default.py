22fd43202d7b32843c4bf383fa742a496a9d9d97
cheungbh <534660436@qq.com>
2021-02-10 10:05:56 +0800

    add pruning training cmds

--- train_cmd_default.py
+++ train_cmd_default.py
@@ -1,31 +1,12 @@
 cmds = [
-    "CUDA_VISIBLE_DEVICES=0 python train_opt.py --backbone mobilenet --struct 0 --expFolder coco_mob_sparse --expID "
-    "sE-6_lrE-3 --loadModel pretrained/mobile_13/86/86_best_acc.pkl --trainBatch 64 --validBatch 64 --LR 1E-3 "
-    "--patience 20 --nEpochs 100 --sparse_s 1E-6 --kps 13 --lr_decay_time 1 --dataset coco --optMethod adam",
-    "CUDA_VISIBLE_DEVICES=0 python train_opt.py --backbone mobilenet --struct 0 --expFolder coco_mob_sparse --expID "
-    "s5E-6_lrE-3 --loadModel pretrained/mobile_13/86/86_best_acc.pkl --trainBatch 64 --validBatch 64 --LR 1E-3 "
-    "--patience 20 --nEpochs 100 --sparse_s 5E-6 --kps 13 --lr_decay_time 1 --dataset coco --optMethod adam",
-
-    "CUDA_VISIBLE_DEVICES=1 python train_opt.py --backbone mobilenet --struct 0 --expFolder coco_mob_sparse --expID "
-    "s5E-7_lrE-3 --loadModel pretrained/mobile_13/86/86_best_acc.pkl --trainBatch 64 --validBatch 64 --LR 1E-3 "
-    "--patience 20 --nEpochs 100 --sparse_s 5E-7 --kps 13 --lr_decay_time 1 --dataset coco --optMethod adam",
-    "CUDA_VISIBLE_DEVICES=1 python train_opt.py --backbone mobilenet --struct 0 --expFolder coco_mob_sparse --expID "
-    "sE-6_lrE-4 --loadModel pretrained/mobile_13/86/86_best_acc.pkl --trainBatch 64 --validBatch 64 --LR 1E-4 "
-    "--patience 20 --nEpochs 100 --sparse_s 1E-6 --kps 13 --lr_decay_time 1 --dataset coco --optMethod adam",
-
-    "CUDA_VISIBLE_DEVICES=2 python train_opt.py --backbone mobilenet --struct 0 --expFolder coco_mob_sparse --expID "
-    "sE-6_lr5E-4 --loadModel pretrained/mobile_13/86/86_best_acc.pkl --trainBatch 64 --validBatch 64 --LR 5E-4 "
-    "--patience 20 --nEpochs 100 --sparse_s 1E-6 --kps 13 --lr_decay_time 1 --dataset coco --optMethod adam",
-    "CUDA_VISIBLE_DEVICES=2 python train_opt.py --backbone mobilenet --struct 0 --expFolder coco_mob_sparse --expID "
-    "s5E-6_lr5E-4 --loadModel pretrained/mobile_13/86/86_best_acc.pkl --trainBatch 64 --validBatch 64 --LR 5E-4 "
-    "--patience 20 --nEpochs 100 --sparse_s 5E-6 --kps 13 --lr_decay_time 1 --dataset coco --optMethod adam",
-
-    "CUDA_VISIBLE_DEVICES=3 python train_opt.py --backbone mobilenet --struct 0 --expFolder coco_mob_sparse --expID "
-    "s5E-7_lr5E-4 --loadModel pretrained/mobile_13/86/86_best_acc.pkl --trainBatch 64 --validBatch 64 --LR 5E-4 "
-    "--patience 20 --nEpochs 100 --sparse_s 5E-7 --kps 13 --lr_decay_time 1 --dataset coco --optMethod adam",
-    "CUDA_VISIBLE_DEVICES=3 python train_opt.py --backbone mobilenet --struct 0 --expFolder coco_mob_sparse --expID "
-    "s5E-7_lr1E-4 --loadModel pretrained/mobile_13/86/86_best_acc.pkl --trainBatch 64 --validBatch 64 --LR 1E-4 "
-    "--patience 20 --nEpochs 100 --sparse_s 5E-7 --kps 13 --lr_decay_time 1 --dataset coco --optMethod adam",
+    "python train_opt.py --dataset coco --expFolder prune_test --expID coco_original --backbone seresnet18",
+    "python train_opt.py --dataset coco --expFolder prune_test --expID coco_s1E-4 --backbone seresnet18 --loadModel ",
+    "python train_opt.py --dataset coco --expFolder prune_test --expID coco_s1E-3 --backbone seresnet18 --loadModel ",
+    "python train_opt.py --dataset coco --expFolder prune_test --expID coco_s5E-4 --backbone seresnet18 --loadModel ",
+    "python train_opt.py --dataset aic --expFolder prune_test --expID aic_original --backbone seresnet18",
+    "python train_opt.py --dataset aic --expFolder prune_test --expID aic_s1E-4 --backbone seresnet18 --loadModel ",
+    "python train_opt.py --dataset aic --expFolder prune_test --expID aic_s1E-3 --backbone seresnet18 --loadModel ",
+    "python train_opt.py --dataset aic --expFolder prune_test --expID aic_s5E-4 --backbone seresnet18 --loadModel ",
 ]
 
 import os
