e81eb60a9ad442d766c4b9af8fc61d3d8325d873
cheungbh <534660436@qq.com>
2021-02-25 17:36:06 +0800

    seresnet18 model structure

--- models/seresnet18/FastPose.py
+++ models/seresnet18/FastPose.py
@@ -5,7 +5,6 @@ from torch.autograd import Variable
 from .layers.SE_Resnet import SEResnet
 from models.duc.DUC import DUC
 from src.opt import opt
-from config.config import train_body_part, device
 
 
 class SeResPose18(nn.Module):
@@ -21,8 +20,8 @@ class SeResPose18(nn.Module):
                 data = file.readlines()
             cfg = data[0].replace("[", "").replace("]", "").replace("\n", "").replace(" ", "").split(",")
             cfg = [int(i) for i in cfg]
-            # if len(cfg) > 10:   # shortcut pruning
-            head_inp, duc1_out, duc2_out = cfg[-3], cfg[-2], cfg[-1]
+            if len(cfg) > 10:   # shortcut pruning
+                head_inp, duc1_out, duc2_out = cfg[-3], cfg[-2], cfg[-1]
 
         self.seresnet18 = SEResnet(cfg=cfg)
 
@@ -30,15 +29,7 @@ class SeResPose18(nn.Module):
         self.duc1 = DUC(int(head_inp/4), duc1_out, upscale_factor=2)
         self.duc2 = DUC(int(duc1_out/4), duc2_out, upscale_factor=2)
 
-        self.conv_out = nn.Conv2d(self.DIM, opt.kps, kernel_size=3, stride=1, padding=1)
-
-        if opt.loadModel:
-            if "duceedt" in opt.loadModel:
-                self.conv_out = nn.Conv2d(self.DIM, 33, kernel_size=3, stride=1, padding=1)
-
-    # def parse_cfg(self, cfg_file):
-    #     with open(cfg_file, "r") as f:
-
+        self.conv_out = nn.Conv2d(int(duc2_out/4), opt.kps, kernel_size=3, stride=1, padding=1)
 
     def forward(self, x: Variable):
         out = self.seresnet18(x)
@@ -53,30 +44,6 @@ class SeResPose18(nn.Module):
 
 def createModel(cfg=None):
     return SeResPose18(cfg)
-
-
-class InferenNet_fast(nn.Module):
-    def __init__(self, dataset="coco", cfg=None):
-        super(InferenNet_fast, self).__init__()
-        if device != "cpu":
-            model = createModel(cfg=cfg).cuda()
-        else:
-            model = createModel(cfg=cfg)
-        print('Loading pose model from {}'.format(opt.loadModel))
-        model.load_state_dict(torch.load(opt.loadModel, map_location=device))
-
-        model.eval()
-        self.pyranet = model
-
-        self.dataset = dataset
-
-    def forward(self, x):
-        out = self.pyranet(x)
-        if opt.loadModel:
-            if "duc" in opt.loadModel:
-                out = out.narrow(1, 0, 17)
-
-        return out
 
 
 def test():
