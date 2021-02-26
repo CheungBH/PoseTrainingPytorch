85173c75dbc25736bc90827f33b2bce6101bc2b5
cheungbh <534660436@qq.com>
2021-02-26 16:58:43 +0800

    change converter into OOP

--- convert.py
+++ convert.py
@@ -4,12 +4,78 @@ import torch
 import os
 from src.opt import opt
 
+
+class Converter:
+    '''Must be assigned if option.pkl is not exist'''
+    backbone = ""
+    height = 256
+    width = 256
+    opt.kps = 13
+    opt.se_ratio = 16
+
+    def __init__(self, model_path, cfg=None, onnx_path="model.onnx", libtorch_path="model.pt", onnx_sim_path="model_sim.onnx", device="cpu"):
+        self.src_model_path = model_path
+        self.onnx_path = onnx_path
+        self.libtorch_path = libtorch_path
+        self.onnx_sim_path = onnx_sim_path
+        self.model_cfg = cfg
+        self.device = device
+        self.superior_path = "/".join(model_path.replace("\\", "/").split("/")[:-1])
+        self.load_options()
+        self.load_model()
+        self.dummy_input = torch.rand(2, 3, self.width, self.height)
+        if self.device == "cpu":
+            self.dummy_input = torch.rand(2, 3, self.width, self.height).cuda()
+
+    def onnx_convert(self):
+        if self.onnx_path:
+            torch_out = torch.onnx.export(self.model, self.dummy_input, self.onnx_path, verbose=False, )
+            if self.onnx_sim_path:
+                import os
+                os.system("python -m onnxsim {} {}".format(self.onnx_path, self.onnx_sim_path))
+
+    def libtorch_convert(self):
+        if self.libtorch_path:
+            traced_model = torch.jit.trace(self.model, self.dummy_input)
+            traced_model.save(self.libtorch_path)
+
+    def load_model(self):
+        posenet = PoseModel()
+        posenet.build(self.backbone, self.model_cfg)
+        posenet.load(self.src_model_path)
+        self.model = posenet.model
+
+    def load_options(self):
+        option_path = get_option_path(self.src_model_path)
+        if os.path.join(option_path):
+            option = torch.load(option_path)
+            self.backbone = option.backbone
+            self.height = option.inputResH
+            self.width = option.inputResW
+            opt.kps = option.kps
+            try:
+                opt.se_ratio = option.se_ratio
+            except:
+                opt.se_ratio = 1
+
+    def convert(self):
+        with torch.no_grad():
+            self.onnx_convert()
+            self.libtorch_convert()
+
+
+if __name__ == '__main__':
+    pytorch_model = "onnx/models_bh_0226/mobile_13/87_best_acc.pkl"
+    convert = Converter(pytorch_model)
+    convert.convert()
+
+'''
 model_path = "onnx/models_bh_0226/seresnet18/aic_original_best_acc.pkl"
 cfg = ""
 onnx_path = "onnx/models_bh_0226/seresnet18/model.onnx"
 libtorch_path = "onnx/models_bh_0226/seresnet18/model.pt"
 
-'''Must be assigned if option.pkl is not exist'''
+# Must be assigned if option.pkl is not exist
 backbone = ""
 height = ""
 width = ""
@@ -41,3 +107,5 @@ with torch.no_grad():
         traced_model.save(libtorch_path)
     if onnx_path:
         torch_out = torch.onnx.export(model, example, onnx_path, verbose=False, )
+
+'''
