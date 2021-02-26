cc76854b94a2e67d25d3aa096cdca64c96589b57
cheungbh <534660436@qq.com>
2021-02-26 17:43:14 +0800

    add seresnet50

---
+++ models/seresnet50/FastPose.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+from .layers.SE_Resnet import SEResnet
+from models.duc.DUC import DUC
+from src.opt import opt
+
+
+class SeResPose50(nn.Module):
+    DIM = 128
+
+    def __init__(self, cfg_file):
+        super(SeResPose50, self).__init__()
+
+        cfg = None
+        head_inp, duc1_out, duc2_out = 512, 256, 128
+        if cfg_file:
+            with open(cfg_file) as file:
+                data = file.readlines()
+            cfg = data[0].replace("[", "").replace("]", "").replace("\n", "").replace(" ", "").split(",")
+            cfg = [int(i) for i in cfg]
+
+        self.preact = SEResnet(cfg=cfg)
+
+        self.suffle1 = nn.PixelShuffle(2)
+        self.duc1 = DUC(512, 1024, upscale_factor=2)
+        self.duc2 = DUC(256, 512, upscale_factor=2)
+
+        self.conv_out = nn.Conv2d(self.DIM, opt.kps, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x: Variable):
+        out = self.preact(x)
+        out = self.suffle1(out)
+        out = self.duc1(out)
+        out = self.duc2(out)
+
+        out = self.conv_out(out)
+
+        return out
+
+
+def createModel(cfg=None):
+    return SeResPose50(cfg)
+
+
+def test():
+    net = createModel()
+    y = net(torch.randn(1,3,64,64))
+    print(net, file=open("FastPose.txt","w"))
+    print(y.size())
+
+
+if __name__ == '__main__':
+    test()
+
