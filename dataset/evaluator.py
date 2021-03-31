17c80f676f51f3f85f9d6f0ec9c0bc033dea1e3a
cheungbh <534660436@qq.com>
2021-03-31 16:37:14 +0800

    add transfoem

---
+++ dataset/evaluator.py
@@ -0,0 +1,22 @@
+
+class BatchEvaluator:
+    def __init__(self):
+        pass
+
+    def eval_per_batch(self):
+        pass
+
+    def update(self):
+        pass
+
+
+class EpochEvaluator:
+    def __init__(self):
+        pass
+
+
+
+
+
+
+
--- dataset/transform.py
+++ dataset/transform.py
@@ -8,6 +8,19 @@ class ImageTransform:
         self.mean = [0.485, 0.456, 0.406]
         self.std = [0.229, 0.224, 0.225]
 
+    def init_with_cfg(self, data_cfg):
+        with open(data_cfg, "r") as load_f:
+            load_dict = data_cfg.load(load_f)
+        self.input_height = load_dict["input_height"]
+        self.input_width = load_dict["input_width"]
+        self.output_height = load_dict["output_height"]
+        self.output_width = load_dict["output_width"]
+        self.sigma = load_dict["sigma"]
+        self.rotate = load_dict["rotate"]
+        self.flip_prob = load_dict["flip_prob"]
+        self.scale = load_dict["scale"]
+        self.kps = load_dict["kps"]
+
     def load_img(self, img_path):
         img = cv2.imread(img_path)
         if self.color == "rgb":
@@ -22,20 +35,28 @@ class ImageTransform:
         img = F.normalize(img, mean=self.mean, std=self.std)
         return img
 
-    def scale(self):
-        pass
+    def scale(self, im, bbox, rate):
+        left, top, width, height = bbox[0], bbox[1], bbox[2], bbox[3]
+        imght = im.shape[1]
+        imgwidth = im.shape[2]
+        x = max(0, left - width * rate / 2)
+        y = max(0, top - height * rate / 2)
+        bottomRightx = min(imgwidth - 1, left + width * (1+rate / 2))
+        bottomRighty = min(imght - 1, top + height * (1+rate / 2))
+        return [x, y, bottomRightx, bottomRighty]
 
-    def flip(self, img, kps):
-        pass
+    def flip(self, img, box, kps):
+        return img, box, kps
 
-    def rotate(self, img, kps):
-        pass
+    def rotate(self, img, kps, rot):
+        return img, kps
 
     def tensor2img(self, ts):
         img = F.to_pil_image(ts)
         return img
 
-
+    def process(self):
+        pass
 
 
 
