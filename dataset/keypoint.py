ed5a1132c10ccd74d9a94deed20874fa19858361
cheungbh <534660436@qq.com>
2021-05-12 10:43:03 +0800

    modify kps

--- dataset/keypoint.py
+++ dataset/keypoint.py
@@ -1,10 +1,10 @@
 #-*-coding:utf-8-*-
-import json
-
 
 class KeyPointsRegister:
     def __init__(self):
-        pass
+        self.base_kps = ['top_head', 'neck', 'nose', 'left eye', 'right eye', 'left ear', 'right ear', 'left shoulder',
+                         'right shoulder', 'left elbow', 'right elbow', 'left wrist', 'right wrist', 'left hip',
+                         'right hip', 'left knee', 'right knee', 'left ankle', 'right ankle']
 
     @staticmethod
     def get_name(num):
@@ -15,26 +15,42 @@ class KeyPointsRegister:
         elif num == 13:
             return ['nose', 'left shoulder', 'right shoulder', 'left elbow', 'right elbow', 'left wrist',
                     'right wrist', 'left hip', 'right hip', 'left knee', 'right knee', 'left ankle', 'right ankle']
+        elif num == 14:
+            return ['top head', 'neck', 'left shoulder', 'right shoulder', 'left elbow', 'right elbow', 'left wrist',
+                    'right wrist', 'left hip', 'right hip', 'left knee', 'right knee', 'left ankle', 'right ankle']
 
 
 class KeyPointsProcessor:
     def __init__(self):
-        self.body_parts_name = ['nose', 'left eye', 'right eye', 'left ear', 'right ear', 'left shoulder',
+        self.coco_parts_name = ['nose', 'left eye', 'right eye', 'left ear', 'right ear', 'left shoulder',
                                 'right shoulder', 'left elbow', 'right elbow', 'left wrist', 'right wrist', 'left hip',
                                 'right hip', 'left knee', 'right knee', 'left ankle', 'right ankle']
+        self.mpii_parts_name = ['top head', 'neck', 'left shoulder', 'right shoulder', 'left elbow', 'right elbow',
+                                'left wrist', 'right wrist', 'left hip', 'right hip', 'left knee', 'right knee',
+                                'left ankle', 'right ankle']
+        self.kps13_parts_name = ['nose', 'left shoulder', 'right shoulder', 'left elbow', 'right elbow', 'left wrist',
+                                 'right wrist', 'left hip', 'right hip', 'left knee', 'right knee', 'left ankle',
+                                 'right ankle']
 
-    def init_kps(self, idx):
+    def init_kps(self, idx, dataset_type):
         if isinstance(idx, int):
-            assert idx == 17 or idx == 13, "Wrong keypoints nums"
+            assert idx == 17 or idx == 13 or idx == 14, "Wrong keypoints nums"
             if idx == 17:
                 self.body_part_idx = [i+1 for i in range(17)]
+                self.body_part_name = self.coco_parts_name
+            elif idx == 14:
+                self.body_part_idx = [i+1 for i in range(14)]
+                self.body_part_name = self.mpii_parts_name
             else:
-                self.body_part_idx = [1, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+                self.body_part_name = self.kps13_parts_name
+                if dataset_type == "mpii" or dataset_type == "aic":
+                    self.body_part_idx = [i+1 for i in range(13)]
+                else:
+                    self.body_part_idx = [1, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
         elif isinstance(idx, list):
-            pass
+            raise NotImplementedError
         else:
             raise TypeError("Your key-point register is wrong! ")
-        self.body_part_name = [self.body_parts[no] for no in self.body_part_idx]
 
     def get_kps_name(self):
         return self.body_part_idx, self.body_part_name
