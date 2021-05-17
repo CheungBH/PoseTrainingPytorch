b904e4d5e5dca7b58ee49570f030e3acb1754a22
SAIL-LAB155 <saillab155@gmail.com>
2021-05-17 10:20:23 +0800

    fix config

--- auto/config.py
+++
@@ -1,8 +0,0 @@
-excel_path = "auto/yoga/resnet18.csv"
-excel_path = excel_path.replace("\\", "/")
-
-task_folder = excel_path.split("/")[-2]
-excel_name = excel_path.split("/")[-1]
-base_name = excel_name.split(".")[0]
-
-computer = "laptop"
