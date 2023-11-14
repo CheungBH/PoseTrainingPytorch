bc5d3a3b84f3aa6884732fa5266f992f52158a65
BookerXU <bookerxu@163.com>
2023-11-14 17:19:16 +0800

    sf

--- image_splitcate.py
+++ image_splitcate.py
@@ -4,21 +4,8 @@ from PIL import Image
 
 json_file = "/media/hkuit164/Backup/xjl/ML_data_process/ML/0206far/fall/project-40-at-2023-10-24-09-20-7576869d/result.json"
 image_folder = "/media/hkuit164/Backup/xjl/ML_data_process/ML/0206far/fall/project-40-at-2023-10-24-09-20-7576869d/images"
-output_folder = "/media/hkuit164/Backup/xjl/ML_data_process/ML/0206far/fall/project-40-at-2023-10-24-09-20-7576869d/fall"
-scale_num = 15
-
-
-def scale(img_h, img_w, bbox, sf):
-    assert len(sf) == 4, "You should assign 4 different factors value"
-    x_min, y_min, x_max, y_max = bbox[0], bbox[1], bbox[2], bbox[3]
-    width, height = x_max - x_min, y_max - y_min
-    # imgheight = img.shape[0]
-    # imgwidth = img.shape[1]
-    x_enlarged_min = max(0, x_min - width * sf[0] / 2)
-    y_enlarged_min = max(0, y_min - height * sf[1] / 2)
-    x_enlarged_max = min(img_w - 1, x_max + width * sf[2] / 2)
-    y_enlarged_max = min(img_h - 1, y_max + height * sf[3] / 2)
-    return [x_enlarged_min, y_enlarged_min, x_enlarged_max, y_enlarged_max]
+output_folder = "/media/hkuit164/Backup/xjl/ML_data_process/ML/0206far/fall/project-40-at-2023-10-24-09-20-7576869d"
+scale_factor = 0.2
 
 
 with open(json_file, "r") as f:
@@ -57,12 +44,15 @@ for image_data in data["images"]:
 
                 image = Image.open(image_path)
 
-                left = max(0, bbox[0] - scale_num)
-                top = max(0, bbox[1] - scale_num)
-                right = min(image.width, bbox[0] + bbox[2] + scale_num)
-                bottom = min(image.height, bbox[1] + bbox[3] + scale_num)
+                scale_factor /= 2
+                width_scale = bbox[2] * scale_factor
+                height_scale = bbox[3] * scale_factor
 
-                # left, top, right, bottom = scale(image.height, image.width, bbox, [0.2, 0.2, 0.2, 0.2])
+                left = max(0, bbox[0] - width_scale)
+                top = max(0, bbox[1] - height_scale)
+                right = min(image.width, bbox[0] + bbox[2] + width_scale)
+                bottom = min(image.height, bbox[1] + bbox[3] + height_scale)
+
 
                 if left < right and top < bottom:
                     cropped_image = image.crop((left, top, right, bottom))
